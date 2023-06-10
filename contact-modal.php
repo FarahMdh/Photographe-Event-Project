65fb8c098840a009dcdcdc5b082ccbbcd88722af
Farah Mdh <mdhfarah3@gmail.com>
2023-06-10 17:28:07 +0200

    Ajout du code CSS et JS de la modale

--- contact-modal.php
+++ contact-modal.php
@@ -1,9 +1,9 @@
 <div class="modal-container">
 
-  <div class="overlay-modal"></div>
+  <div class="overlay modal-trigger"></div>
   
   <div class="modal">
-    <button class="close-modal">X</button>
+    <button class="close-modal modal-trigger">X</button>
     <h1>CONTACTCONTACTCONTACT</h1>
     <?php
 		echo do_shortcode('[contact-form-7 id="56" title="Modale de contact"]');
@@ -12,5 +12,5 @@
 
 </div>
 
-<button class="modal-btn">Open</button>
+<button class="modal-btn modal-trigger">Open</button>
 
--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log ("ça fonctionne")
+
+// apparition / disparition de la modale de contact 
+const modalContainer = document.querySelector(".modal-container");
+const modalTriggers = document.querySelectorAll(".modal-trigger");
+
+modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal))
+
+function toggleModal(){
+    modalContainer.classList.toggle("active")
+
+}
+
