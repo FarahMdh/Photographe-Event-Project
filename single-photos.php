94a9fabe6d77568e5df86aeb10871f6fd07f59d4
Farah Mdh <mdhfarah3@gmail.com>
2023-06-12 20:19:07 +0200

    Ajout des champs personnalisés et taxonomie

--- single-photos.php
+++ single-photos.php
@@ -20,8 +20,47 @@
                 <?php the_content(); ?>
             </div>
 
+            <!-- Champs personnalisés et taxonomie -->
+            <div class="post-reference">
+                <?php echo get_field('reference'); ?>
+            </div>
+
+            <div class="photo-category">
+                <?php
+                $terms = get_the_terms(get_the_ID(), 'categorie');
+                if ($terms && !is_wp_error($terms)) {
+                    foreach ($terms as $term) {
+                        echo $term->name . ' ';
+                    }
+                }
+                ?>
+            </div>
+
+            <div class="post-format">
+                <?php
+                $terms = get_the_terms(get_the_ID(), 'format');
+                if ($terms && !is_wp_error($terms)) {
+                    foreach ($terms as $term) {
+                        echo $term->name . ' ';
+                    }
+                }
+                ?>
+            </div>
+
+            <div class="post-type">
+                <?php echo get_field('type'); ?>
+            </div>
+
+            <div class="post-year">
+                <?php echo get_the_date('Y'); ?>
+            </div>
+
+
             <!-- Display the Contact Button -->
+            <div class="CTA">
+            <p>Cette photo vous intéresse ?</p>
             <a class="contact-button" href="#">Contact</a>
+            </div>
 
         </div>
 
