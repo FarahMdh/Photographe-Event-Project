64c385385bb26e1b6b70ae50f47cb88bea5ebc6e
Farah Mdh <mdhfarah3@gmail.com>
2023-06-13 18:38:32 +0200

    Création de container pour les éléments du post Photos

--- single-photos.php
+++ single-photos.php
@@ -3,73 +3,95 @@
 
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-        <div class="post-content">
+    <div class="post-content">
 
-            <!-- Display the Title -->
-            <h1 class="post-title line-break-title"><?php the_title(); ?></h1>        <!-- pour que les titres aillent à la ligne -->
+        <div class="first-section">
 
-            <!-- Display the Featured Image -->
-            <?php if (has_post_thumbnail()) : ?>
-                <div class="post-thumbnail">
-                    <?php the_post_thumbnail(); ?>
+            <div class="title_ref">
+
+                <div class="title">
+                    <h1 class="post-title line-break-title"><?php the_title(); ?></h1>        <!-- class pour que les titres aillent à la ligne -->
                 </div>
-            <?php endif; ?>
 
-            <!-- Display the Content -->
-            <div class="post-text">
-                <?php the_content(); ?>
+                <div class="ref">
+                    <div class="post-reference">
+                        <?php
+                        $reference = get_field('reference');
+                        if ($reference) {
+                            echo 'Référence : ' . $reference;
+                        }
+                        ?>
+                    </div>
+
+                    <div class="photo-category">
+                        <?php
+                        $terms = get_the_terms(get_the_ID(), 'categorie');
+                        if ($terms && !is_wp_error($terms)) {
+                            echo 'Catégorie : ';
+                            foreach ($terms as $term) {
+                                echo $term->name . ' ';
+                            }
+                        }
+                        ?>
+                    </div>
+
+                    <div class="post-format">
+                        <?php
+                        $terms = get_the_terms(get_the_ID(), 'format');
+                        if ($terms && !is_wp_error($terms)) {
+                            foreach ($terms as $term) {
+                                echo $term->name . ' ';
+                            }
+                        }
+                        ?>
+                    </div>
+
+                    <div class="post-type">
+                        <?php
+                        $type = get_field('type');
+                        if ($type) {
+                            echo 'Type : ' . $type;
+                        }
+                        ?>
+                    </div>
+
+                    <div class="post-year">
+                        <?php echo 'Année : ' . get_the_date('Y'); ?>
+                    </div>
+
+                </div>
             </div>
-
-            <!-- Champs personnalisés et taxonomie -->
-            <div class="post-reference">
-                <?php echo get_field('reference'); ?>
-            </div>
-
-            <div class="photo-category">
-                <?php
-                $terms = get_the_terms(get_the_ID(), 'categorie');
-                if ($terms && !is_wp_error($terms)) {
-                    foreach ($terms as $term) {
-                        echo $term->name . ' ';
-                    }
-                }
-                ?>
-            </div>
-
-            <div class="post-format">
-                <?php
-                $terms = get_the_terms(get_the_ID(), 'format');
-                if ($terms && !is_wp_error($terms)) {
-                    foreach ($terms as $term) {
-                        echo $term->name . ' ';
-                    }
-                }
-                ?>
-            </div>
-
-            <div class="post-type">
-                <?php echo get_field('type'); ?>
-            </div>
-
-            <div class="post-year">
-                <?php echo get_the_date('Y'); ?>
-            </div>
-
-
-            <!-- Display the Contact Button -->
-            <div class="CTA">
-            <p>Cette photo vous intéresse ?</p>
-            <a class="contact-button" href="#">Contact</a>
-            </div>
-
+            
+                <!-- Display the Featured Image -->
+                <div class="images">
+                <?php if (has_post_thumbnail()) : ?>
+                    <div class="post-thumbnail">
+                        <?php the_post_thumbnail(); ?>
+                    </div>
+                <?php endif; ?>
+                </div>
         </div>
 
-    <?php endwhile; else: ?>
+                <!-- Display the Content -->
+                <div class="post-text">
+                    <?php the_content(); ?>
+                </div>
 
-        <p><?php _e('Sorry, this page does not exist.'); ?></p>
+                
+                <!-- Display the Contact Button -->
+                <div class="CTA">
+                <p>Cette photo vous intéresse ?</p>
+                <a class="contact-button" href="#">Contact</a>
+                </div>
 
-    <?php endif; ?>
+    </div>
 
-</div>
+            <?php endwhile; else: ?>
+
+                <p><?php _e('Sorry, this page does not exist.'); ?></p>
+
+            <?php endif; ?>
+
+        </div>
 
 <?php get_footer(); ?>
