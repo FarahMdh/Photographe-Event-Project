dc501bf3ab80cd38bf1ed3b6c6357f826c92791e
Farah Mdh <mdhfarah3@gmail.com>
2023-06-20 23:51:28 +0200

    Création de la grille d'affichage des photos de la page d'accueil

--- home.php
+++ home.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
+
 <?php
 $random_image_args = array(                 // pour obtenir une image aléatoire du catalogue
     'post_type' => 'photos', 
@@ -22,4 +23,31 @@ wp_reset_postdata();
 <div class="hero" style="background-image: url('<?php echo esc_url($background_image_url); ?>');">
     <h1><img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/title.png" alt="title"></h1>
 </div>
+
+
+<div class="thumbnail-container">
+    <?php
+    $args = array(
+        'post_type' => 'photos', 
+        'posts_per_page' => -1,                 // pour afficher toutes les photos
+    );
+
+    $related_query = new WP_Query($args);
+
+    if ($related_query->have_posts()) :
+        while ($related_query->have_posts()) :
+            $related_query->the_post();
+    ?>
+            <div class="thumbnail">
+                <?php the_post_thumbnail("medium"); ?>
+            </div>
+    <?php
+        endwhile;
+        wp_reset_postdata();
+    else :
+        echo "Aucune photo trouvée.";
+    endif;
+    ?>
+</div>
+
 <?php get_footer(); ?>
